283e4e2604a9f449960e89e5ec734bccdf8a1f88
astronomicle <719168@student.dusd.net>
2024-03-11 13:50:33 -0700

    did 1st thing
    did 1st thing and cant import next thing

---
+++ CrowdSimulation/Assets/CrowdCapsuls/Scritps/AIController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIController : MonoBehaviour
+{
+    public GameObject goal;
+    NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = this.GetComponent<NavMeshAgent>();
+        agent.SetDestination(goal.transform.position);
+    }
+}
